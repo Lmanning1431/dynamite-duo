4c282cff41d4197981e6c9e33505ad2c3e871603
LeEric Manning <maanningleeric@gmail.com>
2023-08-16 10:38:41 -0500

    FinalCommit

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Tipo",
+            power: "Super strength",
+
+        },
+        {
+            id: 2,
+            name: "Jiren",
+            power: "Ice blasts"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
